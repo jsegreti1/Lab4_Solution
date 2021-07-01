281ada10ef2846e7d528a6d9b5224c52be65dc2e
tstba <tstba@DESKTOP-OCNM9P0>
2021-07-01 17:46:09 -0400

    Lab #2 solution

--- ScrabbleBLL/src/main/java/pkgCore/Dictionary.java
+++ ScrabbleBLL/src/main/java/pkgCore/Dictionary.java
@@ -60,7 +60,41 @@ public class Dictionary {
 	 * @return
 	 */
 	private boolean match(String first, String second) {
+		try {
 
+			if (second.isEmpty() || second.isBlank())
+				return true;
+
+			// If we reach at the end of both strings,
+			// we are done
+			if (first.length() == 0 && second.length() == 0)
+				return true;
+
+			// Make sure that the characters after '*'
+			// are present in second string.
+			// This function assumes that the first
+			// string will not contain two consecutive '*'
+			if (first.length() > 1 && first.charAt(0) == '*' && second.length() == 0)
+				return false;
+
+			// If the first string contains '?',
+			// or current characters of both strings match
+			if ((first.length() > 1 && first.charAt(0) == '?')
+					|| (first.length() != 0 && second.length() != 0 && first.charAt(0) == second.charAt(0)))
+				return match(first.substring(1), second.substring(1));
+
+			// If there is *, then there are two possibilities
+			// a) We consider current character of second string
+			// b) We ignore current character of second string.
+			if (first.length() > 0 && first.charAt(0) == '*')
+				return match(first.substring(1), second) || match(first, second.substring(1));
+
+			if (first.length() == 1 && first.charAt(0) == '?' && second.length() == 1)
+				return true;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 
 		return false;
